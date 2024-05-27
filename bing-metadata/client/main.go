1b4e28cc057a25cd9a1d90243a4d3189b0ba0f02
anggiariksa <anggia@tokyotreat.com>
2024-05-27 07:22:03 +0700

    print result to file

--- bing-metadata/client/main.go
+++ bing-metadata/client/main.go
@@ -17,32 +17,42 @@ import (
 func handler(i int, s *goquery.Selection) {
 	url, ok := s.Find("a").Attr("href")
 	if !ok {
+		log.Fatalln("error find href")
 		return
 	}
 
 	fmt.Printf("%d: %s", i, url)
 	res, err := http.Get(url)
 	if err != nil {
+		log.Fatalln("error get url")
 		return
 	}
 
 	buf, err := io.ReadAll(res.Body)
 	if err != nil {
+		log.Fatalln("error when read all")
 		return
 	}
 	defer res.Body.Close()
 
 	r, err := zip.NewReader(bytes.NewReader(buf), int64(len(buf)))
 	if err != nil {
+		log.Fatalln("error when read zip")
 		return
 	}
 
 	cp, er, err := metadata.NewProperties(r)
 	if err != nil {
+		log.Fatalln("error reading metadata")
 		return
 	}
 
-	log.Printf("%25s %25s - %s %s\n", er.Creator, er.LastModifiedBy, cp.Application, cp.GetMajorVersion())
+	log.Printf(
+		"%25s %25s - %s %s\n",
+		er.Creator,
+		er.LastModifiedBy,
+		cp.Application,
+		cp.GetMajorVersion())
 }
 
 func main() {
@@ -67,5 +77,26 @@ func main() {
 	}
 
 	s := "html body div#b_content ol#b_results li.b_algo div.b_title h2"
+	fmt.Printf("doc size: %d\n", doc.Size())
+	html, err := doc.Html()
+	if err != nil {
+		log.Fatalln("html error")
+	}
+
+	f, err := os.OpenFile("result.html", os.O_CREATE|os.O_RDWR, 0664)
+	if err != nil {
+		log.Fatalln("error open file")
+	}
+	defer f.Close()
+
+	n, err := f.Write([]byte(html))
+	if n == 0 {
+		log.Fatalln("error write file")
+	}
+
+	if err != nil {
+		log.Fatalln("error write html file")
+	}
+
 	doc.Find(s).Each(handler)
 }
